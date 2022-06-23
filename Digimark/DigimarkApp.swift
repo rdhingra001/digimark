356329bc36291b51acc7fc85d933a46f834a4c4f
Ronit Dhingra <ronit@Dhingra-Family-iMac.local>
2022-06-23 17:21:40 -0400

    set up initial project files

---
+++ Digimark/DigimarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigimarkApp.swift
+//  Digimark
+//
+//  Created by Ronit Dhingra on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct DigimarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
