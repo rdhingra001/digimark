b43e15b690c18e0dc8d98bc3805bcd49bfc1ace1
Ronit Dhingra <ronit@DhingramilyiMac.fios-router.home>
2022-06-24 23:21:17 -0400

    set up dark mode ui on all elements of ui, set up try-out mode on app, missing backend for authentication and note creation

---
+++ Digimark/Views/Authentication/AuthenticateView.swift
@@ -0,0 +1,20 @@
+//
+//  AuthenticateView.swift
+//  Digimark
+//
+//  Created by Ronit Dhingra on 6/24/22.
+//
+
+import SwiftUI
+
+struct AuthenticateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AuthenticateView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthenticateView()
+    }
+}
---
+++ Digimark/Views/Notes/NotesMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  NotesMenuView.swift
+//  Digimark
+//
+//  Created by Ronit Dhingra on 6/24/22.
+//
+
+import SwiftUI
+
+struct NotesMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotesMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotesMenuView()
+    }
+}
